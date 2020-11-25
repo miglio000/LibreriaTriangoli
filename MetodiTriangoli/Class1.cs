d3281fa5bbb04151797b34ff3b501b61218e1a72
miglio000 <gabrielemiglio23@gmail.com>
2020-11-25 12:57:32 +0100

    Add project files.

---
+++ MetodiTriangoli/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MetodiTriangoli
+{
+    public class Class1
+    {
+        public static double InserisciLato()
+        {
+            Console.WriteLine("Inserisci il lato: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            return n;
+        }
+        public static string TipoTriangolo(double n1, double n2, double n3)
+        {
+            string tipotriangolo;
+            if (n1 == n2 && n1 == n3)
+            {
+                tipotriangolo = "Equilatero";
+            }
+            else if (n1 == n2 || n1 == n3 || n2 == n3)
+            {
+                tipotriangolo = "Isoscele";
+            }
+            else
+            {
+                tipotriangolo = "Scaleno";
+            }
+
+
+            return tipotriangolo;
+        }
+        public static double? PerimetroTriangolo(double n1, double n2, double n3)
+        {
+            double? perimetro;
+            if (n1 > 0 || n2 > 0 || n3 > 0)
+            {
+                perimetro = n1 + n2 + n3;
+                return perimetro;
+            }
+            else
+            {
+                perimetro = null;
+                Console.WriteLine("Errore");
+            }
+            return perimetro;
+        }
+    }
+}
---
+++ ProvaTriangoli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MetodiTriangoli;
+
+namespace ProvaTriangoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lato1 = Class1.InserisciLato();
+            double lato2 = Class1.InserisciLato();
+            double lato3 = Class1.InserisciLato();
+            double? perimetro = Class1.PerimetroTriangolo(lato1, lato2, lato3);
+            string tipo_triangolo = Class1.TipoTriangolo(lato1, lato2, lato3);
+            Console.WriteLine($"Il perimetro è {perimetro}, ed è {tipo_triangolo}");
+        }
+    }
+}
